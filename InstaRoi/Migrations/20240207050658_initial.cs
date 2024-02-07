da5fb39395956a52a64fd0d0dd63de42c003df5e
Reo Jefton <jeff10.rj@gmail.com>
2024-02-07 00:15:13 -0500

    Added migration files for InstaROIContext

---
+++ InstaRoi/Migrations/20240207050658_initial.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace InstaRoi.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Element",
+                columns: table => new
+                {
+                    Type = table.Column<string>(maxLength: 255, nullable: false),
+                    investmentFieldID = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK__Element__F9B8A48A1BF64AE0", x => x.Type);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Investment",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Field = table.Column<int>(nullable: false),
+                    investmentType = table.Column<string>(maxLength: 255, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Investment", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "MarketingElements",
+                columns: table => new
+                {
+                    elementID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    conversionPercentage = table.Column<decimal>(type: "decimal", nullable: false),
+                    conversionValue = table.Column<int>(nullable: false),
+                    elementDescription = table.Column<string>(maxLength: 255, nullable: true),
+                    elementName = table.Column<string>(maxLength: 255, nullable: false),
+                    elementType = table.Column<string>(maxLength: 255, nullable: false),
+                    endDate = table.Column<DateTime>(type: "date", nullable: true),
+                    investment1 = table.Column<int>(nullable: true),
+                    investment2 = table.Column<int>(nullable: true),
+                    investment3 = table.Column<int>(nullable: true),
+                    investment4 = table.Column<int>(nullable: true),
+                    investment5 = table.Column<int>(nullable: true),
+                    investment6 = table.Column<int>(nullable: true),
+                    investmentField = table.Column<string>(maxLength: 255, nullable: true),
+                    numberOfLeads = table.Column<int>(nullable: false),
+                    profit = table.Column<decimal>(type: "decimal", nullable: true),
+                    roi = table.Column<decimal>(type: "decimal", nullable: true),
+                    roiPercentage = table.Column<decimal>(type: "decimal", nullable: true),
+                    startDate = table.Column<DateTime>(type: "date", nullable: true),
+                    userID = table.Column<int>(nullable: true),
+                    userName = table.Column<string>(maxLength: 255, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK__Marketin__A4DA082A26D137C7", x => x.elementID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Element");
+
+            migrationBuilder.DropTable(
+                name: "Investment");
+
+            migrationBuilder.DropTable(
+                name: "MarketingElements");
+        }
+    }
+}
---
+++ InstaRoi/Migrations/InstaROIContextModelSnapshot.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using InstaRoi.Models;
+
+namespace InstaRoi.Migrations
+{
+    [DbContext(typeof(InstaROIContext))]
+    partial class InstaROIContextModelSnapshot : ModelSnapshot
+    {
+        protected override void BuildModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "1.1.5")
+                .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+
+            modelBuilder.Entity("InstaRoi.Models.Element", b =>
+                {
+                    b.Property<string>("Type")
+                        .ValueGeneratedOnAdd()
+                        .HasMaxLength(255);
+
+                    b.Property<int>("InvestmentFieldId")
+                        .HasColumnName("investmentFieldID");
+
+                    b.HasKey("Type")
+                        .HasName("PK__Element__F9B8A48A1BF64AE0");
+
+                    b.ToTable("Element");
+                });
+
+            modelBuilder.Entity("InstaRoi.Models.Investment", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd()
+                        .HasColumnName("ID");
+
+                    b.Property<int>("Field");
+
+                    b.Property<string>("InvestmentType")
+                        .IsRequired()
+                        .HasColumnName("investmentType")
+                        .HasMaxLength(255);
+
+                    b.HasKey("Id");
+
+                    b.ToTable("Investment");
+                });
+
+            modelBuilder.Entity("InstaRoi.Models.MarketingElements", b =>
+                {
+                    b.Property<int>("ElementId")
+                        .ValueGeneratedOnAdd()
+                        .HasColumnName("elementID");
+
+                    b.Property<decimal>("ConversionPercentage")
+                        .HasColumnName("conversionPercentage")
+                        .HasColumnType("decimal");
+
+                    b.Property<int>("ConversionValue")
+                        .HasColumnName("conversionValue");
+
+                    b.Property<string>("ElementDescription")
+                        .HasColumnName("elementDescription")
+                        .HasMaxLength(255);
+
+                    b.Property<string>("ElementName")
+                        .IsRequired()
+                        .HasColumnName("elementName")
+                        .HasMaxLength(255);
+
+                    b.Property<string>("ElementType")
+                        .IsRequired()
+                        .HasColumnName("elementType")
+                        .HasMaxLength(255);
+
+                    b.Property<DateTime?>("EndDate")
+                        .HasColumnName("endDate")
+                        .HasColumnType("date");
+
+                    b.Property<int?>("Investment1")
+                        .HasColumnName("investment1");
+
+                    b.Property<int?>("Investment2")
+                        .HasColumnName("investment2");
+
+                    b.Property<int?>("Investment3")
+                        .HasColumnName("investment3");
+
+                    b.Property<int?>("Investment4")
+                        .HasColumnName("investment4");
+
+                    b.Property<int?>("Investment5")
+                        .HasColumnName("investment5");
+
+                    b.Property<int?>("Investment6")
+                        .HasColumnName("investment6");
+
+                    b.Property<string>("InvestmentField")
+                        .HasColumnName("investmentField")
+                        .HasMaxLength(255);
+
+                    b.Property<int>("NumberOfLeads")
+                        .HasColumnName("numberOfLeads");
+
+                    b.Property<decimal?>("Profit")
+                        .HasColumnName("profit")
+                        .HasColumnType("decimal");
+
+                    b.Property<decimal?>("Roi")
+                        .HasColumnName("roi")
+                        .HasColumnType("decimal");
+
+                    b.Property<decimal?>("RoiPercentage")
+                        .HasColumnName("roiPercentage")
+                        .HasColumnType("decimal");
+
+                    b.Property<DateTime?>("StartDate")
+                        .HasColumnName("startDate")
+                        .HasColumnType("date");
+
+                    b.Property<int?>("UserId")
+                        .HasColumnName("userID");
+
+                    b.Property<string>("UserName")
+                        .HasColumnName("userName")
+                        .HasMaxLength(255);
+
+                    b.HasKey("ElementId")
+                        .HasName("PK__Marketin__A4DA082A26D137C7");
+
+                    b.ToTable("MarketingElements");
+                });
+        }
+    }
+}
